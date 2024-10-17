4ff553a676559a1cfbb65b56066c237404e1cf90
Vinícius Bretz <148510167+vinibretz@users.noreply.github.com>
2024-10-17 13:40:23 -0300

    Remove unnecessary pip and pygame files

---
+++ compat.py
@@ -0,0 +1,7 @@
+# Definindo DIRECTION_LTR manualmente
+import pygame
+
+DIRECTION_LTR = 0  # Definindo DIRECTION_LTR manualmente
+
+# Adicionando DIRECTION_LTR ao módulo pygame
+pygame.DIRECTION_LTR = DIRECTION_LTR
